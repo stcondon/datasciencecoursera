32ea5485052efc83467702a26d0730aa8f9c776a
Stephen Condon <stcondon@gmail.com>
2014-07-19 15:25:26 -0700

    reorganized run_analysis, added empty readme

---
+++ GettingCleaning/README.md
@@ -0,0 +1,3 @@
+README
+######
+I decided to run the whole thing in one function so as not to clog up the global environment.  The input of the function run_analysis is a string with the directory where the HAR UCI Dataset is located relative to the working directory.
--- GettingCleaning/run_analysis.R
+++ GettingCleaning/run_analysis.R
@@ -1,8 +1,29 @@
 require(plyr)
 run_analysis <- function(dir = "data/UCI HAR Dataset/") {
+       # First we pull in the activity labels and bind them together. 
+       test_labels <- read.table(paste0(dir, "test/y_test.txt"))
+       train_labels <- read.table(paste0(dir, "train/y_train.txt"))
+       al <- read.table(paste0(dir, "activity_labels.txt"))
+       labels <- rbind(train_labels, test_labels)
+       
+       # We want pretty activity labels, so we first remove the pesky _'s
+       # and then substitute the text from the activity_labels.txt the 
+       # numbers in the the label sets for the activities they represent
+       al$V2 <- gsub("_", " ", al$V2)
+       for(i in seq(6)) {labels$V1 <- gsub(i, al$V2[i], labels$V1)}
+       
+       # Now we introduce the subjects, which we just bind to the activities,
+       # name the columns, being careful with rbind to preserve the order
+       # we used above to bind the activity labels together.
+       subj_train <- read.table(paste0(dir, "train/subject_train.txt"))
+       subj_test <- read.table(paste0(dir, "test/subject_test.txt"))
+       subjects <- rbind(subj_train, subj_test)
+       labels <- cbind(subjects, labels)
+       colnames(labels) <- c("Subject", "Activity")
+       
        # The dataset is rather large, so first instinct is to cut as much out
-       # as possible. First step: find the categories we're interested in,
-       # and excise the rest.
+       # as possible. Our next step has to be to find the categories 
+       # we're interested in, and excise the rest.
        features <- read.table(paste0(dir, "features.txt"))
        feat <- rbind(features[grep("mean", features$V2),], # meanFreq also
                      features[grep("std", features$V2),])
@@ -14,7 +35,14 @@ run_analysis <- function(dir = "data/UCI HAR Dataset/") {
        freq <- feat[grep("Freq", feat$V2),1]
        feat <- feat[!feat$V1 %in% freq,]
        
-       # Now import both datasets, combine them and then use our new
+       # Now to give our feature labels a facelift, we convert them to
+       # camelCase and remove the -'s and ()'s
+       feat$V2 <- gsub("-m", "M", feat$V2)
+       feat$V2 <- gsub("-s", "S", feat$V2)
+       feat$V2 <- gsub("-", "", feat$V2)
+       feat$V2 <- gsub("\\()", "", feat$V2) 
+       
+       # Now we import both datasets, combine them and then use our new
        # features list (feat) to define the columns we actually want,
        # (Takes a LONG time)
        test <- read.table(paste0(dir, "test/X_test.txt"))
@@ -22,42 +50,12 @@ run_analysis <- function(dir = "data/UCI HAR Dataset/") {
        data <- rbind(train,test)
        data <- data[,feat$V1]
        colnames(data) <- feat$V2
-       
-       # Now we pull in the activity labels. We bind them together,
-       # being careful with rbind to preserve the order we used above 
-       test_labels <- read.table(paste0(dir, "test/y_test.txt"))
-       train_labels <- read.table(paste0(dir, "train/y_train.txt"))
-       al <- read.table(paste0(dir, "activity_labels.txt"))
-       labels <- rbind(train_labels, test_labels)
-       
-       # We want pretty activity labels, so we first remove those _'s
-       # and then substitute the text from the activity_labels.txt the 
-       # numbers in the the label sets for the activities they represent
-       al$V2 <- gsub("_", " ", al$V2)
-       for(i in seq(6)) {labels$V1 <- gsub(i, al$V2[i], labels$V1)}
-       
-       # Now we introduce the subjects, which we just bind to the activities,
-       # name the columns, and then bind again to the dataset
-       subj_train <- read.table(paste0(dir, "train/subject_train.txt"))
-       subj_test <- read.table(paste0(dir, "test/subject_test.txt"))
-       subjects <- rbind(subj_train, subj_test)
-       labels <- cbind(subjects, labels)
-       colnames(labels) <- c("Subject", "Activity")
        data <- cbind(labels, data)
        
-       # Now we see use Hadley Wickham's ddply from plyr package to
-       # give us the average measurement for each variable by subject
-       # and activity
+       # Finally we get to use ddply from Hadley Wickham's plyr package to
+       # give us the average measurement for each variable by subject and
+       # activity. And that's it! We can call our data and, when called, 
+       # the function will give us our tidy data!
        data <- ddply(data, .(Subject, Activity), colwise(mean))
-       
-       # And here we have our final data set! To finish up,
-       # we give it a facelift and change all those unsightly
-       # column names to camelCase and lose the -'s and ()'s.
-       colnames(data) <- gsub("-m", "M", colnames(data))
-       colnames(data) <- gsub("-s", "S", colnames(data))
-       colnames(data) <- gsub("-", "", colnames(data))
-       colnames(data) <- gsub("\\()", "", colnames(data))
-       
-       #To conclude, call the dataset
        data
 }
