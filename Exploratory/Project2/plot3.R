c1bb25bf18779094f85948967db6642c6ec02fc9
Stephen Condon <stcondon@gmail.com>
2014-07-22 12:42:32 -0700

    finished up plot3, added png

--- Exploratory/Project2/plot3.R
+++ Exploratory/Project2/plot3.R
@@ -1,4 +1,8 @@
 library(ggplot2)
 library(plyr)
 NEI <- readRDS("data//exdata_data_NEI_data/summarySCC_PM25.rds")
-qplot
+charm_city <- NEI[NEI$fips == "24510", c(4,5,6)]
+charm_city <- ddply(charm_city, .(year, type), colwise(sum))
+png(file = "plot3.png", width = 600)
+qplot(year, Emissions, data = charm_city, geom = "line", facets = . ~ type)
+dev.off()
