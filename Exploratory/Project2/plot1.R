4fb45f85e6d810e44b0adcd5d14337390504aedd
Stephen Condon <stcondon@gmail.com>
2014-07-21 18:39:26 -0700

    added png's for first 2

--- Exploratory/Project2/plot1.R
+++ Exploratory/Project2/plot1.R
@@ -3,6 +3,8 @@ total <- tapply(NEI$Emissions, NEI$year, FUN = sum)
 year <- seq(1999,2008,3)
 total <- total/1000000
 total <- cbind(year, total)
+png(file = "plot1.png")
 plot(total, type = "l", main = "Total US Emissions", xlab = "Year", 
      xaxt = "n", ylab = "PM2.5 Emitted (million tons)")
 axis(1, year)
+dev.off()
--- Exploratory/Project2/plot2.R
+++ Exploratory/Project2/plot2.R
@@ -1,9 +1,12 @@
 NEI <- readRDS("data//exdata_data_NEI_data/summarySCC_PM25.rds")
-charm_city <- NEI[NEI$fips == "06037", c(4,6)]
+charm_city <- NEI[NEI$fips == "24510", c(4,6)]
 charm_city <- tapply(charm_city$Emissions, charm_city$year, FUN = sum)
 charm_city <- charm_city/1000
 year <- seq(1999,2008,3)
 charm_city <- cbind(year,charm_city)
-plot(charm_city, type = "l", main = "Baltimore Emissions",
+png(file = "plot2.png")
+plot(charm_city, type = "n", main = "Baltimore Emissions",
      xlab = "Year", xaxt = "n", ylab = "PM2.5 Emitted (thousand tons)")
+lines(charm_city, type = "l", lwd = 2, col = "orange2"
 axis(1, year)
+dev.off()
