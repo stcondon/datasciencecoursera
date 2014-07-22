ec5b0522858dc7c190fcbf0535b1dad8f8e5daba
Stephen Condon <stcondon@gmail.com>
2014-07-22 13:00:17 -0700

    added ylab

--- Exploratory/Project2/plot3.R
+++ Exploratory/Project2/plot3.R
@@ -4,5 +4,6 @@ NEI <- readRDS("data//exdata_data_NEI_data/summarySCC_PM25.rds")
 charm_city <- NEI[NEI$fips == "24510", c(4,5,6)]
 charm_city <- ddply(charm_city, .(year, type), colwise(sum))
 png(file = "plot3.png", width = 600)
-qplot(year, Emissions, data = charm_city, geom = "line", facets = . ~ type)
+qplot(year, Emissions, data = charm_city, geom = "line", facets = . ~ type,
+      ylab = "PM2.5 Emissions")
 dev.off()
