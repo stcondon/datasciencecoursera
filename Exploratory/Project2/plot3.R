e767fb0acde2636ecd94ef97d49edcd592df88a6
Stephen Condon <stcondon@gmail.com>
2014-07-22 13:26:02 -0700

    fixed 3's ylab, added plot4

--- Exploratory/Project2/plot3.R
+++ Exploratory/Project2/plot3.R
@@ -5,5 +5,5 @@ charm_city <- NEI[NEI$fips == "24510", c(4,5,6)]
 charm_city <- ddply(charm_city, .(year, type), colwise(sum))
 png(file = "plot3.png", width = 600)
 qplot(year, Emissions, data = charm_city, geom = "line", facets = . ~ type,
-      ylab = "PM2.5 Emissions")
+      ylab = "PM2.5 Emissions (tons)")
 dev.off()
---
+++ Exploratory/Project2/plot4.R
@@ -0,0 +1,10 @@
+library(plyr)
+NEI <- readRDS("data//exdata_data_NEI_data/summarySCC_PM25.rds")
+SCC <- readRDS("data//exdata_data_NEI_data/Source_Classification_Code.rds")
+coal_list <- SCC[grep("coal", SCC$Short.Name),1]
+coal <- NEI[NEI$SCC %in% coal_list,c(4,6)]
+coal <- ddply(coal, .(year), colwise(sum))
+png(file = "plot4.png")
+plot(coal, type = "l", main = "Countrywide Coal Emissions", 
+     ylab = "PM2.5 Emissions (tons)")
+dev.off()
