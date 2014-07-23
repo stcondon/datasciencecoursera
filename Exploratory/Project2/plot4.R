9daf8616a984bd2804d36036d4e843c28a6115c6
Stephen Condon <stcondon@gmail.com>
2014-07-22 17:00:45 -0700

    switched looking through Short.Name, now grep through EI.Sector

--- Exploratory/Project2/plot4.R
+++ Exploratory/Project2/plot4.R
@@ -1,7 +1,7 @@
 library(plyr)
 NEI <- readRDS("data//exdata_data_NEI_data/summarySCC_PM25.rds")
 SCC <- readRDS("data//exdata_data_NEI_data/Source_Classification_Code.rds")
-coal_list <- SCC[grep("coal", SCC$Short.Name),1]
+coal_list <- SCC[grep("Coal", SCC$EI.Sector),1]
 coal <- NEI[NEI$SCC %in% coal_list,c(4,6)]
 coal <- ddply(coal, .(year), colwise(sum))
 png(file = "plot4.png")
